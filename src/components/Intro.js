d565b162c3511d7c56d8cbd2212ecd43180d8629
Lena <tsybulenko.helen@gmail.com>
2020-06-15 20:06:12 +0300

    Init App

---
+++ src/components/Intro.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {Switch, Route, Link} from "react-router-dom";
+import icon from "../assets/images/icon-red2.png";
+import Titles from "./constants/Titles";
+import appstoreImage from "../assets/images/appstore.png";
+
+const Intro = () => {
+    return(
+        <div className="intro-text">
+            <div className="app-icon">
+                <Link to="/">
+                    <img src={icon} alt="Thing Happen App" />
+                    <span>Things Happen</span>
+                </Link>
+            </div>
+            <Titles />
+            <div className="h2">Track things you do.</div>
+            <div className="intro-button">
+                <a href="#">
+                    <img src={appstoreImage} alt="Download on the appstore" />
+                </a>
+            </div>
+            <div className="info-links">
+                <a href="mailto:tobuyapp.ios@gmail.com">Contacts</a>
+                <Switch>
+                    <Route path="/react-things-happen" exact>
+                        <Link to="/react-things-happen/privacy-policy">Privacy Policy</Link>
+                    </Route>
+                </Switch>
+            </div>
+        </div>
+    )
+}
+
+export default Intro
